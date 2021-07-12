cb3ef8973088ccfd3ccf0e7473b6d8301c45fe75
William <williamcarpenterfrank@gmail.com>
2021-07-12 15:26:13 +0200

    Ignore last commit

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
-// ------ERROR TYPES------
-// 0: Power operator must succeed number
-// 1: Cannot have two operands next to each other
-
-function calculate(input){
-    let result = 0;
-
-    input = input.replace(/ /g,'');
-    let charSet = input.split("");
-
-    if(isNaN(parseInt(charSet[0]))){
-        return "Must start with number";
+function operate(operator, a, b){
+    switch (operator){
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "/":
+            return divide(a, b);
+        case "*":
+            return multiply(a, b);
     }
-
-    let joinedSet = [];
-
 }
 
-function joinNumsAndPowers(charSet){
-    let joinedSet =[];
-    let addItem = "";
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function divide(a, b){
+    return a/b;
+}
 
-    for(i=0; i<charSet.length; i++){
-        if(!isNan(parseInt(charSet[i]))){
-            addItem += charSet[i]
-        }else if(charSet[i]==="^"){
-            if(isNan(charSet[i-1])){
-                return "0";
-            }
-            addItem+=charSet[i];
-        }else{
-            if(addItem===""){
-                return "1";
-            }
-            joinedSet.push(addItem);
-            addItem = "";
-            addItem+=charSet[i];
-            joinedSet.push(addItem);
-            addItem = "";
-        }
-    }
-    joinedSet.push(addItem);
+function multiply(a, b){
+    return a*b;
 }
